93394acb9e69e727dcc456387f39609b954b5199
sunkeding <970335012@qq.com>
2020-09-03 23:29:15 +0800

    提交本机的brew安装软件

---
+++ brewinstallsoftware.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+brew install p7zip
+brew install jq
+brew install jo
+brew install bsdiff
+brew install ffmpeg
+brew install groovy
+brew install python3@3.8
+#空格预览
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook qlvideo provisionql quicklookapk
