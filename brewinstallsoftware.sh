5a3351815745ddb33b57616a82a2884bf1152abf
sunkeding <970335012@qq.com>
2021-03-14 15:41:54 +0800

    Update brewinstallsoftware.sh

--- brewinstallsoftware.sh
+++ brewinstallsoftware.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
+#Homebrew国内如何自动安装（国内地址）https://zhuanlan.zhihu.com/p/111014448 
+brew cask install WebPQuickLook
+brew install graphviz
+brew install telnet
+brew install watchman
+brew install flow
+brew install openresty/brew/openresty
+brew install pstree
+brew install git-gui
+brew install htop
+brew install ninja
+brew install scrcpy
+brew install jenkins-lts
+brew install qrencode
 brew install p7zip
+brew install node
 brew install jq
 brew install jo
 brew install bsdiff
 brew install ffmpeg
 brew install groovy
 brew install python3@3.8
+brew cask install atom
+brew install tldr
+brew install tig
 #空格预览
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook qlvideo provisionql quicklookapk
